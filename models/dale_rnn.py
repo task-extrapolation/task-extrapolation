7a00c8729460af100b9d653fade8861145c9f6ed
task-extrapolation <task.extrapolation@gmail.com>
2022-10-17 21:05:56 +0000

    Initial commit pathfinder expts

---
+++ models/dale_rnn.py
@@ -0,0 +1,161 @@
+"""Recurrent EI normalization."""
+import numpy as np
+import torch  # pylint: disable=import-error
+import torch.nn as nn  # pylint: disable=import-error
+import torch.nn.functional as F  # pylint: disable=import-error
+from torch.nn import init
+
+
+def nonnegative_weights_init(m):
+    """Non-negative initialization of weights."""
+    if isinstance(m, nn.Conv2d):
+        nn.init.uniform_(m.weight)
+
+
+class DaleRNNcell(nn.Module):
+    """
+    Implements recurrent inhibitory excitatory normalization w/ lateral connections
+    params:
+      input_dim: Number of channels in input
+      hidden_dim: Number of hidden channels
+      kernel_size: Size of kernel in convolutions
+    """
+
+    def __init__(self,
+                 in_channels,
+                 hidden_dim=None,
+                 divnorm_fsize=5,
+                 exc_fsize=7,
+                 inh_fsize=5,
+                 device='cuda',
+                 init_ = None,
+                 ):
+        super(DaleRNNcell, self).__init__()
+        self.in_channels = in_channels
+        if hidden_dim is None:
+            self.hidden_dim = in_channels
+        else:
+            self.hidden_dim = hidden_dim
+        # recurrent gates computation
+        self.g_exc_x = nn.Conv2d(self.in_channels, self.hidden_dim, 1)
+        self.ln_e_x = nn.GroupNorm(num_groups=1, num_channels=self.hidden_dim)
+        self.g_exc_e = nn.Conv2d(self.hidden_dim, self.hidden_dim, 1)
+        self.ln_e_e = nn.GroupNorm(num_groups=1, num_channels=self.hidden_dim)
+        self.g_inh_x = nn.Conv2d(self.in_channels, self.hidden_dim, 1)
+        self.ln_i_x = nn.GroupNorm(num_groups=1, num_channels=self.hidden_dim)
+        self.g_inh_i = nn.Conv2d(self.hidden_dim, self.hidden_dim, 1)
+        self.ln_i_i = nn.GroupNorm(num_groups=1, num_channels=self.hidden_dim)
+        self.ln_out_e = nn.GroupNorm(
+            num_groups=1, num_channels=self.hidden_dim)
+        self.ln_out_i = nn.GroupNorm(
+            num_groups=1, num_channels=self.hidden_dim)
+
+        # feedforward stimulus drive
+        self.w_exc_x = nn.Conv2d(self.in_channels, self.hidden_dim, 1)
+        self.w_inh_x = nn.Conv2d(self.in_channels, self.hidden_dim, 1)
+
+        # horizontal connections (e->e, i->e, i->i, e->i)
+        self.w_exc_ei = nn.Conv2d(
+            self.hidden_dim * 2, self.hidden_dim, exc_fsize, padding=(exc_fsize-1) // 2)
+        # disynaptic inhibition with pairs of E-I cells, E -> exciting surround I -> inhibiting surround E
+        self.w_inh_ei = nn.Conv2d(
+            self.hidden_dim * 2, self.hidden_dim, inh_fsize, padding=(inh_fsize-1) // 2)
+        
+        if init_ == 'ortho':
+            print("initializing RNN weights with Orthogonal Initialization")
+            init.orthogonal(self.g_exc_x.weight)
+            init.orthogonal(self.g_exc_e.weight)
+            init.orthogonal(self.g_inh_x.weight)
+            init.orthogonal(self.g_inh_i.weight)
+
+            init.orthogonal(self.w_exc_x.weight)
+            init.orthogonal(self.w_exc_ei.weight)
+            init.orthogonal(self.w_inh_x.weight)
+            init.orthogonal(self.w_inh_ei.weight)
+
+            init.constant(self.g_exc_x.bias, 0.)
+            init.constant(self.g_exc_e.bias, 0.)
+            init.constant(self.g_inh_x.bias, 0.)
+            init.constant(self.g_inh_i.bias, 0.)
+
+            init.constant(self.w_exc_x.bias, 0.)
+            init.constant(self.w_exc_ei.bias, 0.)
+            init.constant(self.w_inh_x.bias, 0.)
+            init.constant(self.w_inh_ei.bias, 0.)
+
+    def forward(self, input, hidden):
+        exc, inh = hidden
+        g_exc = torch.sigmoid(self.ln_e_x(self.g_exc_x(
+            input)) + self.ln_e_e(self.g_exc_e(exc)))
+        g_inh = torch.sigmoid(self.ln_i_x(self.g_inh_x(
+            input)) + self.ln_i_i(self.g_inh_i(inh)))
+
+        e_hat_t = torch.relu(
+            self.w_exc_x(input) +
+            self.w_exc_ei(torch.cat((exc, inh), 1)))
+
+        i_hat_t = torch.relu(
+            self.w_inh_x(input) +
+            self.w_inh_ei(torch.cat((exc, inh), 1)))
+
+        exc = torch.relu(self.ln_out_e(g_exc * e_hat_t + (1 - g_exc) * exc))
+        inh = torch.relu(self.ln_out_i(g_inh * i_hat_t + (1 - g_inh) * inh))
+        return (exc, inh)
+
+
+class DaleRNNLayer(nn.Module):
+    def __init__(self,
+                 in_channels,
+                 hidden_dim=None,
+                 divnorm_fsize=5,
+                 exc_fsize=11,
+                 inh_fsize=5,
+                 timesteps=4,
+                 device='cuda',
+                 temporal_agg=False,
+                 init_=None,
+                 ):
+        super(DaleRNNLayer, self).__init__()
+        self.in_channels = in_channels
+        self.hidden_dim = hidden_dim
+        self.divnorm_fsize = divnorm_fsize
+        self.exc_fsize = exc_fsize
+        self.inh_fsize = inh_fsize
+        self.timesteps = timesteps
+        self.init_ = init_
+        print("%s steps of recurrence" % self.timesteps)
+        self.device = device
+        self.rnn_cell = DaleRNNcell(in_channels=self.in_channels,
+                                    hidden_dim=self.hidden_dim,
+                                    divnorm_fsize=self.divnorm_fsize,
+                                    exc_fsize=self.exc_fsize,
+                                    inh_fsize=self.inh_fsize,
+                                    device=self.device,
+                                    init_=self.init_)
+        self.emb_exc = nn.Conv2d(self.in_channels, self.hidden_dim, 1)
+        self.emb_inh = nn.Conv2d(self.in_channels, self.hidden_dim, 1)
+        if temporal_agg:
+            self.temporal_agg = nn.Parameter(torch.ones([1, self.timesteps]))
+        else:
+            self.temporal_agg = None
+
+    def forward(self, input):
+        outputs_e = []
+        outputs_i = []
+        n_x, _, w_x, h_x = input.shape
+        # state = (self.emb_exc(input), self.emb_inh(input))
+        # state = (torch.zeros_like(input), torch.zeros_like(input))
+        state = (torch.zeros(n_x, self.hidden_dim, w_x, h_x),
+                torch.zeros(n_x, self.hidden_dim, w_x, h_x))
+        if torch.cuda.is_available():
+            state = (state[0].cuda(), state[1].cuda())
+        for _ in range(self.timesteps):
+            state = self.rnn_cell(input, state)
+            outputs_e += [state[0]]
+            outputs_i += [state[1]]
+        if self.temporal_agg is not None:
+            t_probs = nn.Softmax(dim=1)(self.temporal_agg)
+            outputs_e = torch.stack(outputs_e)
+            output = torch.einsum('ij,jklmn -> iklmn', t_probs, outputs_e)
+            return output[0]
+        return outputs_e[-1]
---
+++ models/model_builder.py
@@ -0,0 +1,142 @@
+"""BaseModel class that builds other models"""
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from models import resnet as models_resnet
+from models.dale_rnn import DaleRNNLayer
+from models.hgru import hConvGRU
+from models.convgru import ConvGRU
+from models.utils import get_gabor_conv
+
+
+class BaseModel(nn.Module):
+    def __init__(self, cfg, args):
+        super(BaseModel, self).__init__()
+        self.cfg = cfg
+        self.in_res = cfg.in_res
+        self.name = cfg.name
+        self.backbone = cfg.backbone[0]
+        self.nlayers = cfg.nlayers
+        self.num_classes = cfg.num_classes
+        self.timesteps = args.timesteps if args.timesteps else self.backbone['timesteps']
+        self.num_ori = 32  # max(32, self.backbone['out_channels'])
+        if self.name.startswith("dalernn"):
+            self.rnn = DaleRNNLayer( #self.backbone['out_channels'],
+                                    self.num_ori,
+                                    self.backbone['out_channels'],
+                                    # timesteps=self.backbone['timesteps'],
+                                    timesteps=self.timesteps,
+                                    exc_fsize=self.backbone['fsize'],
+                                    init_=self.backbone['init_']
+                                    )
+            self.num_units = self.backbone['out_channels']
+
+        elif self.name.startswith("hgru"):
+            self.rnn = hConvGRU(
+                filt_size=self.backbone['fsize'],
+                hidden_dim=self.backbone['out_channels'],
+                timesteps=self.backbone['timesteps'])
+            self.num_units = self.backbone['out_channels']
+
+        elif self.name.startswith("gru"):
+            self.rnn = ConvGRU(self.num_ori,
+                               self.backbone['out_channels'],
+                               self.backbone['fsize'],
+                               self.backbone['timesteps'])
+
+            self.num_units = self.backbone['out_channels']
+        
+        if self.name.startswith('ff'):
+            backbone = []
+            in_channels = 3
+            dilation = self.backbone['dilation']
+            for l_i in range(self.nlayers):
+                if l_i == 0:
+                    backbone.append(get_gabor_conv(3, self.backbone['out_channels'],
+                                                   f_size=11, stride=2))
+                else:
+                    backbone.append(nn.Conv2d(in_channels,
+                                              self.backbone['out_channels'],
+                                              self.backbone['fsize'],
+                                              padding=self.backbone['fsize'] // 2 - 1,
+                                              stride=self.backbone['stride'],
+                                              dilation=dilation
+                                              ))
+                backbone.append(nn.BatchNorm2d(self.backbone['out_channels']))
+                backbone.append(nn.ReLU())
+                if l_i == 0:
+                    backbone.append(nn.MaxPool2d(kernel_size=3,
+                                                 stride=2, padding=1))
+                in_channels = self.backbone['out_channels']
+            self.backbone = nn.Sequential(*backbone)
+
+        elif self.name == 'dalernn' or self.name == 'hgru' or self.name == 'gru' or self.name == 'extrnn':
+            self.backbone = nn.Sequential(
+                get_gabor_conv(3, self.num_ori,
+                               f_size=11, stride=2),
+                nn.BatchNorm2d(self.num_ori),
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(
+                    kernel_size=3, stride=2, padding=1),
+                self.rnn,
+            )
+        elif self.name == 'bn_ff':
+            pass
+        elif self.name == 'gn_ff':
+            pass
+        elif self.name == 'ln_ff':
+            pass
+        elif self.name.startswith('resnet_thin'):
+            self.backbone = models_resnet.__dict__['resnet%s_thin' % self.nlayers](inplanes=self.backbone['out_channels'],
+                                                                                   pretrained=False,
+                                                                                   num_classes=self.num_classes)
+            self.num_units = self.backbone.out_dim
+        elif self.name.startswith('resnet'):
+            self.backbone = models_resnet.__dict__['resnet%s' % self.nlayers](inplanes=self.backbone['out_channels'],
+                                                                                pretrained=False,
+                                                                                num_classes=self.num_classes)
+            self.num_units = self.backbone.out_dim
+        elif self.name == 'r_resnet':
+            pass
+        else:
+            raise NotImplementedError(self.backbone)
+        self.final_conv = nn.Conv2d(self.num_units, 2, 1)
+        self.final_pool = nn.AdaptiveMaxPool2d((1, 1))
+        self.readout = nn.Linear(2, self.num_classes)
+
+    def forward(self, x):
+        if self.name.startswith('resnet'):
+            x = self.backbone.get_intermediate_layers(
+                x, n=1)  # retrieve post-avg-pool output
+        else:
+            x = self.backbone(x)
+        self.rnn_out = x
+        x = self.final_conv(x)
+        x = self.final_pool(x)
+        x = torch.flatten(x, 1)
+        x = self.readout(x)
+        return x
+
+    def get_intermediate_layers(self, x, n):
+        """Get the n'th layer"""
+        if self.name.startswith('resnet'):
+            x = self.backbone.get_intermediate_layers(
+                x, n=1)  # retrieve post-avg-pool output
+        else:
+            x = self.backbone(x)
+        if n == 0:
+            return x
+        x = self.final_conv(x)
+        if n == 1:
+            return x
+        x = self.final_pool(x)
+        if n == 2:
+            return x
+        x = torch.flatten(x, 1)
+        x = self.readout(x)
+        if n == 3:
+            return x
+        return x
